c070aa97cb594d69db1d2ee4e606035e8c5b0874
Дмитрий Платонов <dplatonov@adway.team>
2024-01-29 09:59:16 +0300

    (main) change base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   // base: "/picasso-test/"
-  base: "/",
+  base: "/picasso-test/",
   resolve: {
     alias: {
       "@shared": path.resolve(__dirname, ".src/shared"),
